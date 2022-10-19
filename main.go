90d4aeef01c0ce9bcc96010998bf557cd110b103
Noa Lea <noa@noalea.work>
2022-10-19 09:27:40 -0400

    Store environment variables using viper

--- main.go
+++ main.go
@@ -6,18 +6,18 @@ import (
 
 	"github.com/noalea/simplebank/api"
 	db "github.com/noalea/simplebank/db/sqlc"
+	"github.com/noalea/simplebank/util"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:password@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "localhost:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
@@ -26,7 +26,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
