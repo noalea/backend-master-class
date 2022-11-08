c4bd63b8e1eb6a38d7102572a3c8bf2640f93c94
Noa Lea <noa@noalea.work>
2022-11-08 17:52:07 -0500

    Create users endpoint and hash passwords

---
+++ util/password.go
@@ -0,0 +1,21 @@
+package util
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// HashPassword returns a hashed bcrypt version of the password
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("failed to hash password: %s", password)
+	}
+	return string(hashedPassword), nil
+}
+
+// CheckPassword checks if the password matches the hashedPassword
+func CheckPassword(password string, hashedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
